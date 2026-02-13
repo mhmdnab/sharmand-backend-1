1330f5e3b662b4abbbede27b55e7f3bd3eb91f67
mhmdnab <mhmdnab004@gmail.com>
2026-02-13 21:28:07 +0200

    Refactor db.js: standardize string quotes and improve connection log message

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected');
+    console.log("MongoDB connected !");
   } catch (err) {
     console.error(err.message);
     process.exit(1);
